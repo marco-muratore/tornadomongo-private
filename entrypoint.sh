ed0dad6c859587b8fda9a3bd15259a66f6ef8fa2
MialliEdo <michele.scarlato@edo.io>
2024-04-25 06:03:52 +0200

    [GH Action - docker compose with MongoDB as a service] Added unittest to the entrypoint.sh.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,4 @@
 python3 students_tornado.py &
 sleep 3
 newman run https://api.postman.com/collections/718114-86993b77-21dd-4210-ae54-2630affd0537?access_key=PMAT-01HW2YHXHNF4XYZ3HZCG7E127Q
+python3 -m tornado.testing tests
