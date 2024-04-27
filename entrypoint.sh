399bc81e9ff86b2a79084a456da82d727fc815fe
MialliEdo <michele.scarlato@edo.io>
2024-04-27 12:08:38 +0200

    Integrated Postman tests with MongoDB in the entrypoint.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,6 @@
 
 python3 students_tornado.py &
 sleep 3
-newman run https://api.postman.com/collections/718114-86993b77-21dd-4210-ae54-2630affd0537?access_key=PMAT-01HW2YHXHNF4XYZ3HZCG7E127Q
+# collection with only 2 healthcheck endpoints - without involving MongoDB
+#newman run https://api.postman.com/collections/718114-86993b77-21dd-4210-ae54-2630affd0537?access_key=PMAT-01HW2YHXHNF4XYZ3HZCG7E127Q
+newman run https://api.postman.com/collections/718114-8914ed27-a366-4b19-81a3-1a5cf443d5c0?access_key=PMAT-01HWFE7107BW8K0Q817C3CKD9P
